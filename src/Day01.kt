b107e78fc91529c3eeb60a88a8e52aef2d9f5a04
Ryan Batchelder <ryanbatch@gmail.com>
2022-12-06 18:43:40 -0500

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun main() {
+    fun getElfTotals(input: List<String>): List<Int> {
+        val elfTotals: MutableList<Int> = mutableListOf(0)
+        input.forEach{
+            if (it.isNotBlank()) {
+                elfTotals[elfTotals.lastIndex] = elfTotals.last() + it.toInt()
+            }
+            else {
+                elfTotals.add(0)
+            }
+        }
+        return elfTotals
+    }
     fun part1(input: List<String>): Int {
-        return input.size
+        return getElfTotals(input).maxOf { it }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val elfTotals = getElfTotals(input).sortedDescending()
+        var top3 = 0
+        for (i in 0..2) {
+           top3 += elfTotals[i]
+        }
+        return top3
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println("Part 1 - " + part1(input))
+    println("Part 2 - " + part2(input))
 }
